a0488620b7e2cf22ca06165fd2c7b30206f113ff
Dominik Pataky <pataky@wh2.tu-dresden.de>
2014-03-03 20:32:32 +0100

    Added flask-ldap to code

--- run.py
+++ run.py
@@ -7,18 +7,30 @@ Erstellt am 02.03.2014 von Dominik Pataky pataky@wh2.tu-dresden.de
 """
 
 from flask import Flask, render_template
+from flask.ext.ldap import LDAP
 
 app = Flask(__name__)
 app.secret_key = "q_T_a1C18aizPnA2yf-1Q8(2&,pd5n"
 
+# LDAP config
+app.config["LDAP_DOMAIN"] = "atlantis"
+app.config["LDAP_SEARCH_BASE"] = "ou=buzz,o=AG DSN,c=de???(|(host=atlantis)(host=exorg))"
+app.config["LDAP_PORT"] = 1389
+ldap = LDAP(app)
+app.add_url_rule("/login", "login", ldap.login, methods=["GET", "POST"])
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route("/contacts")
 def contacts():
     return render_template("content/ansprechpartner.html")
+
+#@app.route("/login")
+#def login():
+#    pass
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
