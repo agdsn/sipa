8a52ad457eb1eb78126211ca1ff38fc4273058c0
Dominik Pataky <pataky@wh2.tu-dresden.de>
2014-08-01 15:19:17 +0200

    Add mail.py SMTP functionality

---
+++ mail.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Utils for sending emails via SMTP on localhost.
+"""
+
+from email.utils import formatdate
+from email.mime.text import MIMEText
+import smtplib
+import textwrap
+
+
+def wrap_message(message, chars_in_line=80):
+    """Wraps an unformatted block of text to 80 characters
+    """
+    return_text = []
+    for paragraph in message.split('\n'):
+        lines = textwrap.wrap(paragraph, chars_in_line)
+        if not lines:
+            return_text.append('')
+        else:
+            return_text.extend(lines)
+    return '\n'.join(return_text)
+
+
+def send_mail(sender, receipient, subject, message):
+    """Send a MIME text mail from sender to receipient with subject and message.
+    The message will be wrapped to 80 characters and encoded to UTF8.
+
+    Returns False, if sending from localhost:25 fails.
+    Else returns True.
+    """
+    message = wrap_message(message)
+    mail = MIMEText(message, _charset='utf-8')
+
+    mail['From'] = sender
+    mail['To'] = receipient
+    mail['Subject'] = subject
+    mail['Date'] = formatdate(localtime=True)
+
+    try:
+        smtp = smtplib.SMTP()
+        smtp.connect(host='localhost', port=25)
+        smtp.sendmail(sender, receipient, mail.as_string(0))
+        smtp.close()
+        return True
+    except Exception:
+        # Raises "err", but no official Exception.
+        return False
