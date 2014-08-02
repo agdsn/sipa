b582bdb6926e44d79b408e67eab41bb87d4ab96a
Dominik Pataky <pataky@wh2.tu-dresden.de>
2014-08-02 14:48:24 +0200

    Add timestamp/timetag utilities

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+General utilities
+"""
+
+import time
+
+
+def timetag_from_timestamp(timestamp=time.time()):
+    """Convert a UNIX timestamp to a timetag.
+
+    If timestamp is None, use the current time.
+    COPIED FROM LEGACY
+    """
+    return int(timestamp // 86400)
+
+
+def timestamp_from_timetag(timetag):
+    """Convert a timetag to a UNIX timestamp.
+
+    COPIED FROM LEGACY
+    """
+    return timetag * 86400
