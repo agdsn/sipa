6e29535bd17603dba705c0d3bbcb8b87e21d1bdf
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2015-03-10 00:04:07 +0100

    Fix several issues introduced in the last commit

--- blueprints/pages.py
+++ blueprints/pages.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from exceptions import StopIteration
 
-
-from flask import Blueprint, render_template, session, abort
+from flask import Blueprint, render_template, session, abort, flash
+from flask.ext.babel import gettext
 from jinja2.exceptions import TemplateNotFound
 from flatpages import pages as flat_pages
 
@@ -10,45 +11,16 @@ from flatpages import pages as flat_pages
 bp_pages = Blueprint('pages', __name__, url_prefix='/pages')
 
 
-#def content_localization(template, **kwargs):
-    #"""Some content pages are not suitable to be translated string by string.
-    #In this case we use the whole template in dual language, one template
-    #for german, one for english text.
-    #This method can switch the output template by looking up the language
-    #setting in the session.
-
-    #If the english template is not found (not yet translated) it will
-    #return the german version (lets hope this one's available).
-    #"""
-    #if 'lang' in session and session['lang'] == 'en':
-        #try:
-            #return render_template("content/en/" + template, **kwargs)
-        #except TemplateNotFound:
-            #pass
-    #return render_template("content/" + template, **kwargs)
-
-
-#@bp_pages.route("/contacts")
-#def contacts():
-    #return render_template("content/ansprechpartner.html")
-
-
-#@bp_pages.route("/traffic")
-#def traffic():
-    #return content_localization("traffic.html")
-
-
-@bp_pages.route('/', defaults={'page': 'index'})
+# todo create sitemap at /pages/
+# @bp_pages.route('/', defaults={'page': 'index'})
 @bp_pages.route('/<page>')
 def show(page):
     lang = session.get('lang', 'de')
-    print lang
     flat_pages.reload()
-    try:
-        #page = filter(lambda p: p.path.startswith(lang + u'/'+ page), flat_pages)[0]
-        page = filter(lambda p: p.path.startswith(lang + u'/' + page), flat_pages)[0]
-        return render_template('template.html', page = page)
-    except TemplateNotFound:
-        flash(gettext(u"Seite nicht gefunden!"), "warning")
-    except:
+    page = next((p for p in flat_pages
+                if p.path.startswith(lang + u'/' + page)),
+                None)
+    if page is None:
         abort(404)
+    else:
+        return render_template('template.html', page=page)
