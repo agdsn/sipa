31eec85b00e4a05cc7949028001b09b985b756ec
Peter Klausing <s1856172@mail.zih.tu-dresden.de>
2015-03-13 00:02:20 +0100

    Add custom markdown tables extension
    The included tables extension does not render very nicely with
    Bootstrap as the CSS class table is not applied. This commit adds
    a slightly modified tables extension that does that.

--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@ SECRET_KEY = 'x9d,EFqp1z0()I,D0zE1d#oH6VdFrF'
 
 FLATPAGES_ROOT = 'content'
 FLATPAGES_EXTENSION = '.md'
-FLATPAGES_MARKDOWN_EXTENSIONS = ['sane_lists', 'nl2br', 'meta', 'tables']
+FLATPAGES_MARKDOWN_EXTENSIONS = ['sane_lists', 'Sektionsweb.utils.bootstraped_tables', 'nl2br', 'meta']
