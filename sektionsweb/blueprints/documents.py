f61452fde7e78ed6e56547d515a76fdeb430c312
Peter Klausing <s1856172@mail.zih.tu-dresden.de>
2015-03-17 02:17:18 +0100

    fixed the link to the documents

--- sektionsweb/blueprints/documents.py
+++ sektionsweb/blueprints/documents.py
@@ -10,6 +10,6 @@ bp_documents = Blueprint('documents', __name__, url_prefix='/documents')
 def show(document):
     #(TODO) check wether an document should be avaible<
     try:
-        return send_file('cached_documents/' + document)
+        return send_file('../cached_documents/' + document)
     except IOError:
         abort(404)
