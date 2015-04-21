b6e1c3306ce02e2b3376eb4075196e3731140436
Peter Klausing <peter_klausing@wh2.tu-dresden.de>
2015-04-21 13:53:35 +0200

    Update configuration possibilities
    Configuration ccustom is now made
    through $SIPA_CONFIG_DIR/config.py and overwrites the default_config

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 import os.path
+from os import environ
 from sipa import app
 from sipa.base import init_app
 
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('default_config.py')
+
+# if local config file exists, load everything into local space.
+config_dir = os.getenv('SIPA_CONFIG_DIR', '..')
+
+try:
+    app.config.from_pyfile( '{}/config.py'.format(config_dir))
+except IOError:
+    print("No Config found")
 
 app.config['FLATPAGES_ROOT'] = os.path.join(
 os.path.dirname(os.path.abspath(__file__)), 'content')
