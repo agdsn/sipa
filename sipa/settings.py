841ca67343577ea7607c416c25e4df6a1fb079f2
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2015-04-24 23:00:37 +0200

    Move log file to ./error.log temporarily
    Because you need root access to log to /var/log, which is an
    unnecessary effort, logging should be done into ./error.log for the
    sake of temporary simplicity.

--- sipa/settings.py
+++ sipa/settings.py
@@ -2,7 +2,7 @@ from sipa.config import FLASK_SECRET_KEY
 
 SECRET_KEY = FLASK_SECRET_KEY
 
-LOG_FILE = '/var/log/sipa/error.log'
+LOG_FILE = 'error.log'
 
 FLATPAGES_EXTENSION = '.md'
 FLATPAGES_MARKDOWN_EXTENSIONS = [
