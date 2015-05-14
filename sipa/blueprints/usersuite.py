770c74c3d852caff9f6c75263ee75fa79acd546b
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2015-05-14 17:10:33 +0200

    Move some exception handling to backend
    `change_email()` and many others now catch exceptions by themselves and re-raiss
    them so the frontend function can display the user a message or decide what to
    do.

--- sipa/blueprints/usersuite.py
+++ sipa/blueprints/usersuite.py
@@ -112,11 +112,8 @@ def usersuite_change_password():
             try:
                 change_password(current_user.uid, old, new)
             except PasswordInvalid:
-                logger.info('Wrong password provided when attempting '
-                            'change of password')
                 flash(gettext(u"Altes Passwort war inkorrekt!"), "error")
             else:
-                logger.info('Password successfully changed')
                 flash(gettext(u"Passwort wurde geändert"), "success")
                 return redirect(url_for(".usersuite"))
     elif form.is_submitted():
@@ -141,19 +138,13 @@ def usersuite_change_mail():
 
         try:
             change_email(current_user.uid, password, email)
-        except UserNotFound as e:
-            logger.error('UserNotFound raised in change_email() '
-                         'with current_user. Args: {}'.format(e.args))
+        except UserNotFound:
             flash(gettext(u"Nutzer nicht gefunden!"), "error")
         except PasswordInvalid:
-            logger.info('Wrong password provided when attempting '
-                        'change of mail address')
             flash(gettext(u"Passwort war inkorrekt!"), "error")
         except LDAPConnectionError:
-            logger.error('Not sufficient rights to change the mail address')
             flash(gettext(u"Nicht genügend LDAP-Rechte!"), "error")
         else:
-            logger.info('Mail address successfully changed to {}'.format(email))
             flash(gettext(u"E-Mail-Adresse wurde geändert"), "success")
             return redirect(url_for('.usersuite'))
     elif form.is_submitted():
@@ -175,28 +166,13 @@ def usersuite_delete_mail():
 
         try:
             change_email(current_user.uid, password, "")
-        except UserNotFound as e:
-            logger.error('UserNotFound raised in change_email() '
-                         'with current_user. Args: {}'.format(e.args))
+        except UserNotFound:
             flash(gettext(u"Nutzer nicht gefunden!"), "error")
         except PasswordInvalid:
-            # todo catch these in the backend functions && reraise
-            # log messages should not be handled by the
-            # function responsible for evaluating a HTTP request. This should
-            # happen in the backend. example:
-            # except PasswordInvalid:
-            #     logger.info('stuff')
-            #     raise
-            # especially in *this case*, *one* function is wrapped *twice* with
-            # basically the same exception handling
-            logger.info('Wrong password provided when attempting '
-                        'reset of mail address')
             flash(gettext(u"Passwort war inkorrekt!"), "error")
         except LDAPConnectionError:
-            logger.error('Not sufficient rights to change the mail address')
             flash(gettext(u"Nicht genügend LDAP-Rechte!"), "error")
         else:
-            logger.info('Mail address successfully reset')
             flash(gettext(u"E-Mail-Adresse wurde zurückgesetzt"), "success")
             return redirect(url_for('.usersuite'))
     elif form.is_submitted():
@@ -220,12 +196,10 @@ def usersuite_change_mac():
         try:
             authenticate(current_user.uid, password)
         except PasswordInvalid:
-            logger.info('Wrong password provided when attempting '
-                        'change of MAC address')
             flash(gettext(u"Passwort war inkorrekt!"), "error")
         else:
-            logger.info('Successfully changed MAC address to {}'.format(mac))
             update_macaddress(userinfo['ip'], userinfo['mac'], mac)
+            logger.info('Successfully changed MAC address to {}'.format(mac))
 
             subject = u"[Usersuite] %s hat seine/ihre MAC-Adresse " \
                       u"geändert" % current_user.uid
