aef183870d018c44dbb847cc7dafe6fca87cc9b7
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2015-09-10 17:59:42 +0200

    Simplify design of traffic chart
    - Make background transparent
    
    - Use default fonts
    
    - Use a different color scheme than shades of blue.  Now, it is
      green-yellow-blue for down-up-sum, tweaked a little bit to not look
      extremely colorful, which was done using tones having te same distance
      in the hue value.

--- sipa/utils/graph_utils.py
+++ sipa/utils/graph_utils.py
@@ -2,9 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import pygal
-from pygal.style import BlueStyle
+from pygal.style import Style
+from pygal.colors import hsl_to_rgb
+
 from _locale import gettext
 from operator import add
+
+
+def rgb_string(r, g, b):
+    return "#{:02X}{:02X}{:02X}".format(int(r), int(g), int(b))
+
+
+def hsl(h, s, l):
+    return rgb_string(*hsl_to_rgb(h, s, l))
+
+
+traffic_style = Style(
+    background='transparent',
+    plot_background='transparent',
+    opacity='.6',
+    opacity_hover='.9',
+    transition='200ms ease-in',
+    colors=(hsl(130, 80, 60), hsl(70, 80, 60), hsl(190, 80, 60)),
+    font_family='default'
+)
 
 
 def generate_traffic_chart(traffic_data, inline=True):
@@ -23,16 +44,19 @@ def generate_traffic_chart(traffic_data, inline=True):
         human_readable=False,
         major_label_font_size=12,
         label_font_size=12,
-        style=BlueStyle,
+        style=traffic_style,
         disable_xml_declaration=inline,   # for direct html import
         js=[],  # prevent automatically fetching scripts from github
     )
 
     days, in_values, out_values, credit = zip(*traffic_data['history'])
     traffic_chart.x_labels = days
-    traffic_chart.add(gettext('Input'), in_values)
-    traffic_chart.add(gettext('Output'), out_values)
-    traffic_chart.add(gettext('Gesamt'), map(add, in_values, out_values))
+    traffic_chart.add(gettext('Input'), in_values,
+                      stroke_style={'dasharray': '5'})
+    traffic_chart.add(gettext('Output'), out_values,
+                      stroke_style={'dasharray': '5'})
+    traffic_chart.add(gettext('Gesamt'), map(add, in_values, out_values),
+                      stroke_style={'width': '2'})
 
     return traffic_chart
 
