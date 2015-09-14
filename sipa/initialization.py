f8baa07089d4799c4d55f07e7c0cb3f604f14ca9
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2015-09-14 20:04:18 +0200

    Move ReverseProxied to initialization.py
    It is now used in `init_app` itself rather on `__init__` itself.

--- sipa/initialization.py
+++ sipa/initialization.py
@@ -24,7 +24,7 @@ from sipa.utils.git_utils import init_repo, update_repo
 
 def create_app(name=None):
     app = Flask(name if name else __name__)
-    # TODO: call init_app
+    init_app(app)
     return app
 
 
@@ -37,6 +37,7 @@ def init_app(app):
     * registering the Jinja global variables
     :return: None
     """
+    app.wsgi_app = ReverseProxied(app.wsgi_app)
     init_env_and_config(app)
     logger.debug('Initializing app')
     login_manager.init_app(app)
@@ -141,3 +142,37 @@ def init_logging(app):
         logger.debug("Sentry DSN: {}".format(app.config['SENTRY_DSN']))
     else:
         logger.debug("No sentry DSN specified")
+
+
+class ReverseProxied(object):
+    """Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    """
+    def __init__(self, flask_app):
+        self.app = flask_app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
