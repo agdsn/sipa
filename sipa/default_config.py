3e945521ebb2251166542c9edc4e9ddf8556b9b3
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2015-09-16 00:10:50 +0200

    Fix typo in default_config
    The `default_config.py` is interpreted as ASCII. Therefore, no umlauts
    in comments.

--- sipa/default_config.py
+++ sipa/default_config.py
@@ -10,7 +10,7 @@ which is a file not tracked in git containing IPs, user names, passwords, etc.
 
 from os import getenv
 
-# TODO: Remove os.getenv-Gedöns
+# TODO: Remove os.getenv-Gedoens
 SECRET_KEY = getenv("SIPA_SECRET_KEY")
 
 SENTRY_DSN = getenv('SIPA_SENTRY_DSN', '')
