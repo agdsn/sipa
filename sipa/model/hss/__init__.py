b37bbf3ca691df6508ca708e1a7f8b819aefc347
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2015-11-24 21:53:11 +0100

    Add webmailer addresses

--- sipa/model/hss/__init__.py
+++ sipa/model/hss/__init__.py
@@ -6,6 +6,8 @@ from ..datasource import PrematureDataSource, Dormitory
 datasource = PrematureDataSource(
     name='hss',
     website_url="https://wh12.tu-dresden.de",
+    # to be included when it becomes a DataSource
+    # webmailer_url="https://wh12.tu-dresden.de/roundcube/",
     support_mail="support@wh12.tu-dresden.de",
 )
 
