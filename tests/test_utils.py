c474238dcf0fbd470dd6ba02955a68de9a8c265a
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-02-21 19:09:51 +0100

    Use `set`, not `list` for output comparison
    Using `list` would cause (under certain circumstances) the test to fail,
    just because the position of the returned elements in the list is
    different.  Obviously, this is not what we want to test.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,7 +15,7 @@ class TestDictDiff(TestCase):
     def test_diffs_same_dicts(self):
         dicts = [{}, {'foo': 'bar'}, {'foo': 'bar', 'baz': {'boom': 'sheesh'}}]
         for d in dicts:
-            self.assertEqual(list(dict_diff(d, d)), [])
+            self.assertEqual(set(dict_diff(d, d)), set())
 
     def test_diffs_one_different(self):
         # the dicts in `dicts` *MUST NOT* have two dicts with keys
@@ -26,7 +26,7 @@ class TestDictDiff(TestCase):
         for d1, d2 in permutations(dicts, 2):
             merged = d1.copy()
             merged.update(d2)
-            self.assertEqual(list(dict_diff(d1, merged)), list(d2.keys()))
+            self.assertEqual(set(dict_diff(d1, merged)), set(d2.keys()))
 
 
 class TestHandlerReplacer(TestCase):
