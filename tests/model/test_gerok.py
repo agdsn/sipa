b612bd94c8d6d9c0923d21c3e2eedd70cc3b7e95
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-02-21 20:46:07 +0100

    Add first test && structure for gerok.User tests
    - Add the test structure, i.e. `TestGerokUser` class with example user,
      `User` comparison method and api mock
    
    - Add test testing an explicit creation via `__init__`

--- tests/model/test_gerok.py
+++ tests/model/test_gerok.py
@@ -3,7 +3,7 @@ from itertools import chain
 from requests import Response
 from unittest.mock import MagicMock, patch
 
-from sipa.model.gerok.user import do_api_call
+from sipa.model.gerok.user import do_api_call, User
 from tests.prepare import AppInitialized
 
 
@@ -113,3 +113,70 @@ class TestGerokApiCall(AppInitialized):
         tokens = ["", "foobar123", "dtrndturiaehc",
                   "54TRNEDr:-)/nyUfeg n:s lvℕΓΦ∃Δ∂ℝ⇐⊂6"]
         self.assert_token_passed(tokens, method='post')
+
+
+class TestGerokUser(AppInitialized):
+    users = {
+        '1245': {
+            'login': "test",
+            'address': "Gerokstraße 38, 00-0",
+            'mail': "test@test.de",
+            'status': "OK",
+            'hosts': [
+                {'ip': "141.30.0.0", 'mac': "aa-bb-cc-dd-ee-ff",
+                 'hostname': "foobar", 'alias': "mein_laptop"},
+                {'ip': "141.30.0.1", 'mac': "aa-bb-cc-dd-ee-fe",
+                 'hostname': "baz", 'alias': "mein_neuer_laptop"},
+            ],
+        },
+    }
+    api_mock = MagicMock()
+
+    def setUp(self):
+        def fake_api(request, method='get', postdata=None):
+            """A fake gerok api, replacing `do_api_call` for testing."""
+            # TODO: read get-arguments.  Currently, only trivial request
+            # cases (request == user_id, return a dict with user data) are
+            # handled.
+            return {
+                'id': request,
+                **self.users.get(request)
+            }
+
+        self.api_mock.side_effect = fake_api
+
+    def get_example_user(self, id):
+        try:
+            return {
+                'id': id,
+                **self.users.get(id)
+            }
+        except KeyError:
+            raise ValueError("Id {} not in `users` dict".format(id))
+
+    def assert_userdata_passed(self, user, user_data):
+        """Test if contents of `user_data` fully appear in the `user` object
+        """
+        self.assertEqual(user.uid, user_data['login'])
+        self.assertEqual(user.id.value, user_data['id'])
+        self.assertEqual(user.login.value, user_data['login'])
+        self.assertEqual(user.address.value, user_data['address'])
+        self.assertEqual(user.mail.value, user_data['mail'])
+        self.assertEqual(user.status.value, user_data['status'])
+        for host in user_data['hosts']:
+            # using `assertIn` allows agnosticism regarding the chosen
+            # string concatenation method
+            self.assertIn(host['ip'], user.ips.value)
+            self.assertIn(host['mac'], user.mac.value)
+            self.assertIn(host['hostname'], user.hostname.value)
+            self.assertIn(host['alias'], user.hostalias.value)
+
+    @patch('sipa.model.gerok.user.do_api_call', api_mock)
+    def test_explicit_init(self):
+        id = '1245'
+        user_data = self.get_example_user(id)
+        user = User(user_data['login'], id)
+
+        self.assertTrue(self.api_mock.called)
+        self.assertEqual(self.api_mock.call_args[0], (id,))
+        self.assert_userdata_passed(user=user, user_data=user_data)
