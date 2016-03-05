c2a2bf6cfcc3950a493ccd6b60b306cad0f9d2aa
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-03-05 02:20:33 +0100

    Simple config updating in `AppInitialized`
    Subclasses of `AppInitialized` now can easily pass an additional
    configuration dict to `AppInitialized.create_app`.

--- tests/prepare.py
+++ tests/prepare.py
@@ -6,11 +6,13 @@ from sipa.defaults import WARNINGS_ONLY_CONFIG
 
 
 class AppInitialized(TestCase):
-    def create_app(self):
+    def create_app(self, additional_config=None):
         test_app = Flask(__name__)
         test_app.config['TESTING'] = True
         test_app.config['LOG_CONFIG'] = WARNINGS_ONLY_CONFIG
         test_app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
         test_app.debug = True
+        if additional_config:
+            test_app.config.update(additional_config)
         test_app = create_app(app=test_app)
         return test_app
