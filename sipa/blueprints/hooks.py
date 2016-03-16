6a8260a377005d2708eb19d23e0ef62cb4f87ba5
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-03-16 04:21:27 +0100

    Add hook blueprint + endpoint

---
+++ sipa/blueprints/hooks.py
@@ -0,0 +1,42 @@
+import logging
+
+from flask import current_app, request, abort
+from flask.blueprints import Blueprint
+
+from sipa.utils.git_utils import update_repo
+
+
+logger = logging.getLogger(__name__)
+
+bp_hooks = Blueprint('hooks', __name__, url_prefix='/hooks')
+
+
+@bp_hooks.route('/update-content', methods=['PUT'])
+def content_hook():
+    auth_key = current_app.config.get('GIT_UPDATE_HOOK_TOKEN')
+
+    if not auth_key:
+        # no key configured (default) → feature not enabled
+        abort(404)
+
+    key = request.args.get('token')
+    if not key:
+        abort(401)
+
+    if key != auth_key:
+        abort(403)
+
+    reload_necessary = update_repo(current_app.config['FLATPAGES_ROOT'])
+    if reload_necessary:
+        try:
+            import uwsgi
+        except ImportError:
+            logger.debug("UWSGI not present, skipping reload")
+            pass
+        else:
+            uwsgi.reload()
+            logger.debug("UWSGI Reloaded")
+
+    # 204: No content
+    # https://en.wikipedia.org/wiki/List_of_HTTP_status_codes#204
+    return "", 204
