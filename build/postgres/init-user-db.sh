98c1dbac010ae4e77fad63945b5fcbc0b7c8a9f8
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-06-22 02:47:41 +0200

    Use sql file, not shell script for pg-container
    Refs #219

--- build/postgres/init-user-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE userman;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO $POSTGRES_USER;
-EOSQL
