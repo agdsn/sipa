e5c0a410a23302370d560bcf6d3e6d5746f3b3e0
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-08-19 19:33:28 +0200

    Add Test{s,base} for endpoints w/ forms
    A test mixin which, given an endpoint, a template and sets of valid and
    invalid form data, conveniently tests whether the access to this page
    works correctly.

--- tests/blueprints/test_bp_generic.py
+++ tests/blueprints/test_bp_generic.py
@@ -3,6 +3,8 @@ from functools import partial
 from flask import abort, url_for
 from tests.base import SampleFrontendTestBase
 
+from sipa.model import backends
+
 
 class TestErrorhandlersCase(SampleFrontendTestBase):
     used_codes = [401, 403, 404]
@@ -59,3 +61,99 @@ class GenericEndpointsReachableTestCase(SampleFrontendTestBase):
     def test_version_reachable(self):
         self.assert200(self.client.get(url_for('generic.version')))
         self.assertTemplateUsed('version.html')
+
+
+class FormTemplateTestMixin:
+    """A Mixin for conveniently testing forms.
+
+    Must be mixed with a subclass of :py:cls:`TestCase` in order to
+    access the assert methods.
+
+    Requires the following attributes:
+
+        - client: The flask test client
+
+        - url: The url of the endpoint to test
+
+        - template: The location of the template file, relative to the
+          template root
+    """
+    def assert_something_flashed(self, data, level='danger'):
+        """Assert that something flashed inside the body.
+
+        :param binary data: binary content of the response
+        :param str level: the flash level, i.e. one of ['info',
+            'success', 'warning', 'danger']
+        """
+        string_to_find = "sipa_flash alert alert-{}".format(level)
+        self.assertIn(string_to_find.encode('utf-8'), data)
+
+    def assert_nothing_flashed(self, data):
+        string_not_to_find = "sipa_flash alert"
+        self.assertNotIn(string_not_to_find.encode('utf-8'), data)
+
+    def test_endpoint_reachable(self):
+        self.assert200(self.client.get(self.url))
+        self.assertTemplateUsed(self.template)
+
+    def test_empty_request_flashes(self):
+        resp = self.client.post(self.url)
+        self.assert_something_flashed(resp.data)
+
+    def test_invalid_data_flashes(self):
+        for data in self.invalid_data:
+            with self.subTest(data=data):
+                resp = self.client.post(self.url, data=data)
+                self.assert_something_flashed(resp.data)
+
+    def test_valid_data_passes(self):
+        for data in self.valid_data:
+            with self.subTest(data=data):
+                resp = self.client.post(self.url, data=data)
+                self.assert_nothing_flashed(resp.data)
+
+
+class AnonymousContactTestCase(FormTemplateTestMixin, SampleFrontendTestBase):
+    def setUp(self):
+        super().setUp()
+        self.url = url_for('generic.contact')
+        self.dormitory = 'localhost'
+        self.template = 'anonymous_contact.html'
+
+        self.valid_data = [{
+            'email': "foo@bar.baz",
+            'name': "Darc net",
+            'dormitory': self.dormitory,
+            'subject': "Test",
+            'message': "Test message!",
+        }]
+        self.invalid_data = [
+            {**self.valid_data[0], 'email': ''},
+            {**self.valid_data[0], 'email': 'foo@bar'},
+            {**self.valid_data[0], 'name': ''},
+            {**self.valid_data[0], 'dormitory': 'not_'+self.dormitory},
+            {**self.valid_data[0], 'subject': ''},
+            {**self.valid_data[0], 'message': ''},
+        ]
+
+
+class OfficialContactTestCase(FormTemplateTestMixin, SampleFrontendTestBase):
+    def setUp(self):
+        super().setUp()
+        self.url = url_for('generic.contact_official')
+        self.template = 'official_contact.html'
+        self.dormitory = 'localhost'
+
+        self.valid_data = [{
+            'email': "foo@bar.baz",
+            'name': "Darc net",
+            'subject': "Test",
+            'message': "Test message!",
+        }]
+        self.invalid_data = [
+            {**self.valid_data[0], 'email': ''},
+            {**self.valid_data[0], 'email': 'foo@bar'},
+            {**self.valid_data[0], 'name': ''},
+            {**self.valid_data[0], 'subject': ''},
+            {**self.valid_data[0], 'message': ''},
+        ]
