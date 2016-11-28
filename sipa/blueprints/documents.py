2eeef8dde4a1821278317790fa4f33a3ed1df6b7
Stefan Haller <stefan.haller@tu-dresden.de>
2016-11-28 11:07:30 +0100

    Introduce restricted documents area
    All documents in the sub-directory “restricted/” will require the user
    to be authenticated.

--- sipa/blueprints/documents.py
+++ sipa/blueprints/documents.py
@@ -3,16 +3,21 @@
 import os
 
 from flask import Blueprint, send_from_directory, current_app
+from flask_login import current_user
 from flask.views import View
 
 bp_documents = Blueprint('documents', __name__)
 
 
 class StaticFiles(View):
-    def __init__(self, directory):
+    def __init__(self, directory, login_required=False):
         self.directory = directory
+        self.login_required = login_required
 
     def dispatch_request(self, filename):
+        if self.login_required and not current_user.is_authenticated:
+            return current_app.login_manager.unauthorized()
+
         if os.path.isabs(self.directory):
             directory = self.directory
         else:
@@ -30,3 +35,9 @@ bp_documents.add_url_rule('/images/<path:filename>',
 bp_documents.add_url_rule('/documents/<path:filename>',
                           view_func=StaticFiles.as_view('show_document',
                                                         '../content/documents'))
+
+
+bp_documents.add_url_rule('/documents/restricted/<path:filename>',
+                          view_func=StaticFiles.as_view('show_document_restricted',
+                                                        '../content/documents/restricted',
+                                                        True))
