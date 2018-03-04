agdsn/sipa

commit 9e3ec85c15b8ca109fe3056fba7065d8c04ac2e9

Use generic.index if no endpoint available
During error handling, no endpoint is available. Fall back to the main index
endpoint in this case.

Fixes #355

sipa/utils/__init__.py
@@ -97,10 +97,12 @@ def url_self(**values):
     :return: A URL to the current endpoint
     """
     if request.endpoint is None:
-        raise RuntimeError("No endpoint available.")
+        endpoint = request.endpoint
+    else:
+        endpoint = 'generic.index'
     kw = request.view_args.copy()
     kw.update(values)
-    return url_for(request.endpoint, **kw)
+    return url_for(endpoint, **kw)
 
 
 def redirect_url(default='generic.index'):

commit d3efad456d47b3f8d03ed26f27f74fcaa6f9f57f

Fix wrong branching in url_self
Introduced by 9e3ec85c15b8ca109fe3056fba7065d8c04ac2e9 to fix #355

sipa/utils/__init__.py
@@ -97,9 +97,9 @@ def url_self(**values):
     :return: A URL to the current endpoint
     """
     if request.endpoint is None:
-        endpoint = request.endpoint
-    else:
         endpoint = 'generic.index'
+    else:
+        endpoint = request.endpoint
     kw = request.view_args.copy()
     kw.update(values)
     return url_for(endpoint, **kw)