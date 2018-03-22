831adcb2051fcd9d847edddcd940818eaab45367
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2018-03-22 18:08:07 +0100

    Simplify static file endpoints

--- sipa/blueprints/documents.py
+++ sipa/blueprints/documents.py
@@ -3,50 +3,37 @@
 import os
 
 from flask import Blueprint, send_from_directory, current_app
-from flask_login import current_user
-from flask.views import View
-
-from sipa.base import login_manager
+from flask.blueprints import BlueprintSetupState
+from flask_login import login_required
 
 
-bp_documents = Blueprint('documents', __name__)
+class StaticFilesBlueprint(Blueprint):
+    def add_static_files(self, rule, endpoint, directory, decorators=()):
+        def add(state: BlueprintSetupState):
+            app = state.app
+            nonlocal directory
+            if not os.path.isabs(directory):
+                directory = os.path.join(app.root_path, directory)
+
+            def send_static_file(filename):
+                cache_timeout = app.get_send_file_max_age(filename)
+                return send_from_directory(directory, filename,
+                                           cache_timeout=cache_timeout)
+
+            for decorator in decorators:
+                send_static_file = decorator(send_static_file)
+
+            app.add_url_rule(rule, endpoint, send_static_file)
+
+        self.record(add)
 
 
-class StaticFiles(View):
-    def __init__(self, directory, login_required=False, member_required=False):
-        self.directory = directory
-        self.login_required = login_required
-        self.member_required = member_required
-
-    def dispatch_request(self, filename):
-        if self.login_required and not current_user.is_authenticated:
-            return current_app.login_manager.unauthorized()
-
-        if self.member_required and not current_user.is_member:
-            return current_app.login_manager.unauthorized()
-
-        if os.path.isabs(self.directory):
-            directory = self.directory
-        else:
-            directory = os.path.join(current_app.root_path, self.directory)
-        cache_timeout = current_app.get_send_file_max_age(filename)
-        return send_from_directory(directory, filename,
-                                   cache_timeout=cache_timeout)
-
-
-bp_documents.add_url_rule('/images/<path:filename>',
-                          view_func=StaticFiles.as_view('show_image',
-                                                        '../content/images'))
-login_manager.ignore_endpoint('documents.show_image')
-
-bp_documents.add_url_rule('/documents/<path:filename>',
-                          view_func=StaticFiles.as_view('show_document',
-                                                        '../content/documents'))
-login_manager.ignore_endpoint('documents.show_document')
-
-
-bp_documents.add_url_rule('/documents_restricted/<path:filename>',
-                          view_func=StaticFiles.as_view('show_document_restricted',
-                                                        '../content/documents_restricted',
-                                                        login_required=True,
-                                                        member_required=True))
+bp_documents = StaticFilesBlueprint('documents', __name__)
+bp_documents.add_static_files('/images/<path:filename>', 'show_image',
+                              '../content/images')
+bp_documents.add_static_files('/documents/<path:filename>', 'show_document',
+                              '../content/documents')
+bp_documents.add_static_files('/documents_restricted/<path:filename>',
+                              'show_document_restricted',
+                              '../content/documents_restricted',
+                              [login_required])
