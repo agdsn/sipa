8361f0545ad257b68ab93845bdf02088b82e7a7c
Lukas Juhrich <lukas.juhrich@agdsn.de>
2018-07-29 19:31:34 +0200

    Fix imports from recent refactor

--- sipa/model/pycroft/__init__.py
+++ sipa/model/pycroft/__init__.py
@@ -2,9 +2,9 @@
 
 from ipaddress import IPv4Network
 
-from ..datasource import DataSource, Dormitory
+from sipa.backends import DataSource, Dormitory
+from sipa.backends.exceptions import InvalidConfiguration
 from . import user
-from sipa.model.exceptions import InvalidConfiguration
 
 
 def init_context(app):
