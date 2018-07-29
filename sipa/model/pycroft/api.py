807aef51c67642cc6a43acc98c83c65cfe10675e
Lukas Juhrich <lukas.juhrich@agdsn.de>
2018-07-29 19:25:57 +0200

    Simplify get and post invocation in pycroft API

--- sipa/model/pycroft/api.py
+++ sipa/model/pycroft/api.py
@@ -12,48 +12,46 @@ class PycroftApi():
         self._api_key = api_key
 
     def get_user(self, username):
-        return self._do_api_call('user/{}'.format(username))
+        return self.get('user/{}'.format(username))
 
     def get_user_from_ip(self, ip):
-        return self._do_api_call('/user/from-ip', method='get', params={'ip': ip})
+        return self.get('/user/from-ip', params={'ip': ip})
 
     def authenticate(self, username, password):
-        return self._do_api_call('user/authenticate', 'post',
-                                 data={'login': username, 'password': password})
+        return self.post('user/authenticate',
+                         data={'login': username, 'password': password})
 
     def change_password(self, user_id, old_password, new_password):
-        return self._do_api_call('user/{}/change-password'.format(user_id), 'post',
-                                 data={'password': old_password,
-                                       'new_password': new_password})
+        return self.post('user/{}/change-password'.format(user_id),
+                         data={'password': old_password,
+                               'new_password': new_password})
 
     def change_mail(self, user_id, password, new_mail):
-        return self._do_api_call('user/{}/change-email'.format(user_id), 'post',
-                                 data={'password': password, 'new_email': new_mail})
+        return self.post('user/{}/change-email'.format(user_id),
+                         data={'password': password, 'new_email': new_mail})
 
     def change_cache_usage(self, user_id, use_cache):
-        return self._do_api_call('user/{}/change-cache-usage'.format(user_id), 'post',
-                                 data={'use_cache': use_cache})
+        return self.post('user/{}/change-cache-usage'.format(user_id),
+                         data={'use_cache': use_cache})
 
-    def _do_api_call(self, request, method='get', params=None, data=None):
-        """Send a request to the Pycroft api
-        """
+    def get(self, url, params=None):
+        request_function = partial(requests.get, params=params or {})
+        return self._do_api_call(request_function, url)
 
-        if method == 'get':
-            request_function = partial(requests.get, params=params)
-        elif method == 'post':
-            request_function = partial(requests.post, data=data)
-        else:
-            raise ValueError("`method` must be one of ['get', 'post']!")
+    def post(self, url, data=None):
+        request_function = partial(requests.get, data=data or {})
+        return self._do_api_call(request_function, url)
 
+    def _do_api_call(self, request_function, url):
         try:
             response = request_function(
-                self._endpoint + request,
+                self._endpoint + url,
                 verify=False,
                 headers={'Authorization': 'ApiKey {}'.format(self._api_key)},
             )
         except ConnectionError as e:
             logger.error("Caught a ConnectionError when accessing Pycroft API",
-                         extra={'data': {'endpoint': self._endpoint + request}})
+                         extra={'data': {'endpoint': self._endpoint + url}})
             raise ConnectionError("Pycroft API unreachable") from e
 
         if response.status_code not in [200, 400, 401, 403, 404]:
