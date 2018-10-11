15bc2b4a9e4b5c5c7310908a25ca701567f092e5
Jakob Müller <j.mueller@outlook.de>
2018-10-11 10:33:52 +0200

    Increase time in warning when changing MAC address

--- sipa/blueprints/usersuite.py
+++ sipa/blueprints/usersuite.py
@@ -296,7 +296,7 @@ def change_mac():
                                'tags': {'rate_critical': True}})
 
             flash(gettext("MAC-Adresse wurde geändert!"), 'success')
-            flash(gettext("Es kann bis zu 10 Minuten dauern, "
+            flash(gettext("Es kann bis zu 24 Stunden (!) dauern, "
                           "bis die Änderung wirksam ist."), 'info')
 
             return redirect(url_for('.index'))
