1f5870818e78ccd9e92b430cc1cd968dfe063bb6
Lukas Juhrich <lukas.juhrich@agdsn.de>
2018-11-11 21:50:05 +0100

    Fix constructor lookup

--- sipa/model/pycroft/unserialize.py
+++ sipa/model/pycroft/unserialize.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from typing import Any, Dict
+import inspect
+import sys
+from typing import Callable, Optional, Any, List
 
 
 class UnserializationError(Exception):
@@ -24,7 +26,70 @@ def _maybe_setattr(cls, attrname, attr):
     setattr(cls, attrname, attr)
 
 
+MAXDEPTH = 100
+
+
+def constructor_from_generic(name: str, args: tuple, *a, **kw) -> Optional[Callable]:
+    """
+    :param name:  Something like 'List' (like from `List[int]._name`)
+    :param args: Something like `(int,)` from (`List[int].__args__`)
+    :return: A constructor callable or None
+    """
+    if name == Any._name:
+        def constructor(val):
+            return val
+
+    elif name == List._name:
+        if len(args) == 1:
+            item_constructor = constructor_from_annotation(args[0], *a, **kw)
+
+            def constructor(val):
+                return [item_constructor(i) for i in val]
+        else:
+            raise UnserializationError("Cannot find constructor for List[A, ...]"
+                                       " with more than one argument.")
+    else:
+        raise UnserializationError(f"Generic type '{name}' not supported")
+
+    return constructor
+
+
+def constructor_from_annotation(type_, module, maxdepth=MAXDEPTH) -> Callable:
+    """Use the value of an annotation to return an appropriate constructor"""
+    if maxdepth <= 0:
+        raise UnserializationError(
+            "Maxdepth exceeded when trying to lookup constructors."
+            f" the default is {MAXDEPTH}."
+        )
+
+    # un-stringification by eval'ing in module scope
+    if isinstance(type_, str):
+        try:
+            type_ = eval(type_, module.__dict__, None)
+        except NameError:
+            raise UnserializationError(f"Unable to look up type {type_!r}"
+                                       f" in module {module.__name__!r}")
+
+    constructor: Optional[Callable] = None
+
+    # Case 1: known generic
+    if hasattr(type_, '_name'):
+        # TODO: make everything else required, except if we get Optional[...] here
+        constructor = constructor_from_generic(type_._name, getattr(type_, '__args__', ()),
+                                               module=module, maxdepth=maxdepth-1)
+
+    # cases 2, 3: Is an unserializer or something builtin
+    elif inspect.isclass(type_):
+        constructor = type_
+
+    if not constructor:
+        raise UnserializationError(f"Could not find constructor for type {type_!r}")
+
+    return constructor
+
+
 def unserializer(cls: type) -> type:
+    """A class decorator providing a magic __init__ method"""
     annotations = {canonicalize_key(key): val
                    for key, val in getattr(cls, '__annotations__', {}).items()}
     setattr(cls, '__annotations__', annotations)
@@ -38,23 +103,29 @@ def unserializer(cls: type) -> type:
 
     def __init__(self, dict_like: dict):
         # TODO read `Optional` attributes
-        missing_keys = self._json_keys - set(dict_like.keys())
+        module = sys.modules[type(self).__module__]
+        constructor_map = {key: constructor_from_annotation(type_=val,
+                                                            module=module)
+                           for key, val in self.__annotations__.items()}
+
+        missing_keys = set(constructor_map.keys()) - set(dict_like.keys())
         if missing_keys:
             raise MissingKeysError(f"Missing {len(missing_keys)} keys:"
                                    f" {', '.join(missing_keys)}")
         # TODO perhaps warn on superfluous keys
 
-        for attrname in self._json_keys:
+        for attrname, constructor in constructor_map.items():
             val = dict_like[attrname]
-            type_ = self.__annotations__[attrname]
-            if type_ != Any:
-                try:
-                    converted = type_(val)
-                except (TypeError, ValueError) as e:
-                    raise ConversionError(f"Failed to convert {val!r}"
-                                          f" to type {type_!r}") from e
-            else:
+            if not constructor:
                 converted = val
+            else:
+                try:
+                    converted = constructor(val)
+                except (TypeError, ValueError) as e:
+                    typename = self.__annotations__[attrname]
+                    raise ConversionError(f"Failed to convert {val!r}"
+                                          f" to type {typename!r}") from e
+
             self.__dict__[attrname] = converted
 
     _maybe_setattr(cls, '__init__', __init__)
