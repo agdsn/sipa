f23bb79e2438a3fbbe6d2f0a54abf9a3d1ca0234
Jakob Müller <j.mueller@outlook.de>
2019-12-18 23:53:32 +0100

    Improve hints

--- sipa/static/js/agdsn.js
+++ sipa/static/js/agdsn.js
@@ -2,37 +2,6 @@ function get_language() {
     return JSON.parse(document.getElementById('locale').innerHTML);
 }
 
-if (window.location.pathname.endsWith("contact")) {
-    var hints = [];
-    $.getJSON("static/js/hints.json", function (raw_hints) {
-        hints = raw_hints.map(function (hint) {
-            return {
-                not_in_dorm: hint.not_in_dorm,
-                patterns: hint.patterns.map(function (pattern) {
-                    return new RegExp(pattern, "i");
-                }),
-                hint: hint.hint
-            };
-        });
-    });
-    $("#message").on("input", function () {
-        var applicable = hints.filter(function (hint) {
-            var contains = hint.patterns.some(function (pattern) {
-                return pattern.test($("#message").val())
-            });
-            var not_blacklisted = hint.not_in_dorm.every(function (dorm) {
-                return dorm !== $("#dormitory").val();
-            });
-            return contains & not_blacklisted;
-        });
-        $("#hints").empty();
-        applicable.forEach(function (hint) {
-            var hint_text = hint.hint[get_language()];
-            $("#hints").append("<div class='alert alert-warning'>" + hint_text + "</div>");
-        });
-    });
-}
-
 //Status widget
 var initStatus = function (components) {
     var content = '',
---
+++ sipa/static/js/contact-hints.js
@@ -0,0 +1,30 @@
+var hints = [];
+$.getJSON("/sipa/static/js/hints.json", function (raw_hints) {
+    hints = raw_hints.map(function (hint) {
+        return {
+            not_in_dorm: hint.not_in_dorm,
+            patterns: hint.patterns.map(function (pattern) {
+                return new RegExp(pattern, "i");
+            }),
+            hint: hint.hint
+        };
+    });
+
+    console.log(hints);
+});
+$("#message").on("input", function () {
+    var applicable = hints.filter(function (hint) {
+        var contains = hint.patterns.some(function (pattern) {
+            return pattern.test($("#message").val())
+        });
+        var not_blacklisted = hint.not_in_dorm.every(function (dorm) {
+            return dorm !== $("#dormitory").val();
+        });
+        return contains & not_blacklisted;
+    });
+    $("#hints").empty();
+    applicable.forEach(function (hint) {
+        var hint_text = hint.hint[get_language()];
+        $("#hints").append("<div class='alert alert-warning'>" + hint_text + "</div>");
+    });
+});
