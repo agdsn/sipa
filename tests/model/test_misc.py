c0b0c40683f4ca74197f21bbbe6624cea35e3959
Lukas Juhrich <lukas.juhrich@agdsn.de>
2023-02-21 16:14:57 +0100

    [tests] migrate `model.test_misc` to pytest #450

--- tests/model/test_misc.py
+++ tests/model/test_misc.py
@@ -1,9 +1,9 @@
-from unittest import TestCase
+import pytest
 
 from sipa.utils import compare_all_attributes, xor_hashes
 
 
-class CompareAllAttributesTestCase(TestCase):
+class TestCompareAllAttributes:
     class A:
         a = 'foo'
         b = 'bar'
@@ -19,39 +19,42 @@ class CompareAllAttributesTestCase(TestCase):
         b = 'shizzle'
         c = 'baz'
 
-    def test_true_comparisons(self):
-        arglist = [
-            (self.A, self.B, ['a', 'b']),
-            (self.A, self.C, ['a', 'c']),
-        ]
-        for args in arglist:
-            assert compare_all_attributes(*args)
+    @pytest.mark.parametrize(
+        "a, b, args",
+        [
+            (A, B, ["a", "b"]),
+            (A, C, ["a", "c"]),
+        ],
+    )
+    def test_true_comparisons(self, a, b, args):
+        assert compare_all_attributes(a, b, args)
 
-    def test_false_comparisons(self):
-        arglist = [
-            (self.A, self.B, ['a', 'c']),
-            (self.A, self.C, ['a', 'b']),
-            (self.A, self.B, ['a', 'b', 'c']),
-            (self.A, self.C, ['a', 'b', 'c']),
-        ]
-        for args in arglist:
-            assert not compare_all_attributes(*args)
+    @pytest.mark.parametrize(
+        "a, b, args",
+        [
+            (A, B, ["a", "c"]),
+            (A, C, ["a", "b"]),
+            (A, B, ["a", "b", "c"]),
+            (A, C, ["a", "b", "c"]),
+        ],
+    )
+    def test_false_comparisons(self, a, b, args):
+        assert not compare_all_attributes(a, b, args)
 
     def test_attributes_missing_false(self):
         """Comparing to an object without these attrs should be `False`"""
         try:
             assert not compare_all_attributes(self.A, "", ["a"])
         except AttributeError:
-            self.fail("AttributeError raised instead of returning `False`")
+            pytest.fail("AttributeError raised instead of returning `False`")
 
 
-class XorHashesTestCase(TestCase):
-    def test_xor_hashes_correct(self):
-        a = "foo"
-        b = "bar"
-        c = "baz"
-        d = 19
-        e = True
-        f = None
-        expected = hash(a) ^ hash(b) ^ hash(c) ^ hash(d) ^ hash(e) ^ hash(f)
-        assert xor_hashes(a, b, c, d, e, f) == expected
+def test_xor_hashes_correct():
+    a = "foo"
+    b = "bar"
+    c = "baz"
+    d = 19
+    e = True
+    f = None
+    expected = hash(a) ^ hash(b) ^ hash(c) ^ hash(d) ^ hash(e) ^ hash(f)
+    assert xor_hashes(a, b, c, d, e, f) == expected
