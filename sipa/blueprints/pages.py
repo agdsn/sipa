20e2e429821f19b7f5d9ea25ee2c3438fa37e0a7
Lukas Juhrich <lukas.juhrich@agdsn.de>
2023-03-08 18:55:48 +0100

    Remove dynamic data support in sipa content
    This is untested and hasn't been used since 2015.

--- sipa/blueprints/pages.py
+++ sipa/blueprints/pages.py
@@ -2,14 +2,11 @@
 Blueprint for the flatpages
 """
 
-import os.path
 from logging import getLogger
-import json
 
 from flask import Blueprint, render_template, redirect, current_app
 from flask_login import current_user
 
-from sipa.backends.extension import backends
 
 logger = getLogger(__name__)
 
@@ -36,57 +33,4 @@ def show(category_id, article_id):
     if article.link:
         return redirect(article.link)
 
-    box_filename = os.path.join(
-        os.path.abspath(current_app.config['FLATPAGES_ROOT']),
-        f"{article.localized_page.path}.json"
-    )
-
-    dynamic_data = load_dynamic_json(box_filename)
-
-    if not dynamic_data:
-        return render_template('template.html', article=article,
-                               dynamic=False)
-
-    return render_template('template.html', article=article,
-                           default_dormitory=backends.preferred_dormitory_name(),
-                           dynamic=True, **dynamic_data)
-
-
-def load_dynamic_json(filename):
-    try:
-        with open(filename, encoding="utf-8") as f:
-            json_string = f.read()
-    except OSError:
-        return
-
-    try:
-        dynamic_data = json.loads(json_string)
-    except ValueError:
-        logger.error("Corrupt json json_file: %s", filename, extra={'data': {
-            'json_string': json_string,
-        }}, exc_info=True)
-        return
-
-    try:
-        keys = dynamic_data['keys']
-        values = dynamic_data['values']
-        mappings = dynamic_data['mappings']
-        title = dynamic_data['title']
-    except KeyError:
-        logger.error("Corrupt json json_file: %s", filename, extra={
-            'data': {'json_content': dynamic_data},
-        }, exc_info=True)
-
-    values = {
-        dorm.name: values[mappings[dorm.name]]
-        for dorm in backends.dormitories
-        if dorm.name in mappings
-    }
-    dormitories = [(dorm.name, dorm.display_name)
-                   for dorm in backends.dormitories
-                   if dorm.name in mappings]
-
-    return {'title': title,
-            'keys': keys,
-            'dormitories': dormitories,
-            'values': values}
+    return render_template("template.html", article=article, dynamic=False)
