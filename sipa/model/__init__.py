559ecfcbae0a87206d7ec57f95c61e472ed0b1f5
Lukas Juhrich <lukas.juhrich@agdsn.de>
2023-03-08 13:45:53 +0100

    Delete flask-sqlalchemy
    Sipa does not have its own SQL model anymore.
    
    Fixes #447

--- sipa/model/__init__.py
+++ sipa/model/__init__.py
@@ -2,7 +2,6 @@ import logging
 
 from sipa.backends import Backends
 from . import sample, pycroft
-from .sqlalchemy import db
 
 
 logger = logging.getLogger(__name__)
@@ -15,16 +14,10 @@ AVAILABLE_DATASOURCES = [
 ]
 
 
-def prepare_sqlalchemy(app):
-    app.config['SQLALCHEMY_BINDS'] = {}
-    db.init_app(app)
-
-
 def build_backends_ext() -> Backends:
     backends = Backends()
     for d in AVAILABLE_DATASOURCES:
         backends.register(d)
 
     # pre_init_hook is designed to be a decorator, so this may look weird.
-    backends.pre_init_hook(prepare_sqlalchemy)
     return backends
