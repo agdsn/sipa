2bb9e3051237ba477a53303d95be5df727173e58
agames4 <robokatze182@gmx.de>
2023-09-09 20:56:42 +0200

    updated control mappings
    updated the control mapping for better usage und easier overview

--- sipa/blueprints/control.py
+++ sipa/blueprints/control.py
@@ -12,31 +12,32 @@ from flask_wtf import FlaskForm
 from markupsafe import Markup
 
 bp_control = Blueprint('control', __name__, url_prefix='/control')
-@bp_control.route("/port", methods=['GET', 'POST'])
+@bp_control.route("/port", methods=['POST'])
 def check_port():
     """
     returns json with all the port forwardings
     """
-    if not request.form.get("port"):
-        return 'port nicht in Textfeld'
-    if not request.form["port"].isnumeric():
-        return gettext('der Port muss eine Nummer sein')
-    if int(request.form["port"]) < 1:
-        return gettext('der Port muss größer als 0 sein')
-    if int(request.form["port"]) > 65535:
-        return gettext('der Port muss kleiner als 65536 sein')
+    for i in request.form:
+        if not request.form.get(i):
+            return 'port nicht in Textfeld'
+        if not request.form[i].isnumeric():
+            return gettext('der Port muss eine Nummer sein')
+        if int(request.form[i]) < 1:
+            return gettext('der Port muss größer als 0 sein')
+        if int(request.form[i]) > 65535:
+            return gettext('der Port muss kleiner als 65536 sein')
     return ""
 
-@bp_control.route("/ip", methods=['GET', 'POST'])
+@bp_control.route("/ip", methods=['POST'])
 def checks_ip_address():
     """
     checks rather the given ip address is valid
     """
 
-    if not request.form.get("source_ip"):
+    if not request.form.get("ip_address"):
         return "das Feld muss eine ip addresse enthalten"
     try:
-        ip = ipaddress.ip_address(request.form.get("source_ip"))
+        ip = ipaddress.ip_address(request.form.get("ip_address"))
     except ValueError:
         return "die IP scheint keine valide IP Adresse zu sein"
     network = ipaddress.ip_network("192.168.10.0/24")
@@ -46,4 +47,7 @@ def checks_ip_address():
 
 @bp_control.route('/delete', methods=['DELETE'])
 def delete():
+    """
+    returns emtpy string for simple deleting things
+    """
     return ""
