d014e77f610b3263f3d6bccbc7024ee5252460df
agames4 <robokatze182@gmx.de>
2023-09-14 22:41:58 +0200

    ip test
    valid ip test

--- tests/blueprints/test_control.py
+++ tests/blueprints/test_control.py
@@ -25,13 +25,13 @@ def client(module_test_client) -> TestClient:
         ],
     )
 def test_invalid_ip(client, ip_responses):
-        resp = client.post("/control/ip",data={"ip_address": ip_responses[0]})
+        resp = client.post("/control/ip", data={"ip_address": ip_responses[0]})
         assert resp.status_code == 200
         assert ip_responses[1] in resp.data.decode("UTF-8")
 
 
 def test_valid_ip(client):
-    for i in range(1,254):
+    for i in range(1, 254):
         ip = "192.168.10." + str(i)
         resp = client.post("/control/ip", data={"ip_address": ip})
         assert resp.status_code == 200
@@ -56,3 +56,10 @@ def test_invalid_port(client, port):
     resp = client.post("/control/port", data={"port": port[0]})
     assert resp.status_code == 200
     assert port[1] in resp.data.decode("UTF-8")
+
+
+def test_valid_port(client):
+    for i in range(1, 65536):
+        resp = client.post("/control/port", data={"port": i})
+        assert resp.status_code == 200
+        assert "" == resp.data.decode("UTF-8")
