0f244e2f919e3c15239d40bb04523e1993a87965
Gregor Düster <git@gdstr.eu>
2023-10-02 13:30:16 +0200

    Remove dynamic content remains

--- sipa/static/js/dormitory.js
+++
@@ -1,6 +0,0 @@
-$("#dorm-select").change(function(){
-    selected = $("select#dorm-select").find(":selected").prop("value");
-    $(".dynamic-content").hide();
-    $("#dynamic-" + selected).show();
-});
-$("#dorm-select").on("load", function() {}).triggerHandler("change")
