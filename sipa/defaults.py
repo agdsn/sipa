342f2a57830cc432ee504f9de5b698d25a6ae3f2
Lukas Juhrich <lukas.juhrich@agdsn.de>
2023-10-19 13:58:53 +0200

    Be less annoying even with debug logging

--- sipa/defaults.py
+++ sipa/defaults.py
@@ -1,37 +1,24 @@
 DEFAULT_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ("%(levelname)s %(asctime)s %(module)s "
-                       "%(process)d %(thread)d %(message)s")
-        },
-        'medium': {
-            'format': ("%(levelname)s %(asctime)s %(module)s "
-                       "%(message)s")
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "medium": {"format": ("%(levelname)s %(asctime)s %(name)s " "%(message)s")},
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "stdout": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "medium",
         },
     },
-    'handlers': {
-        'stdout': {
-            'class': 'logging.StreamHandler',
-            'level': 'DEBUG',
-            'formatter': 'verbose',
+    "loggers": {
+        "sipa": {
+            "level": "DEBUG",
+            "handlers": ["stdout"],
+            "propagate": True,  # Important for `sipa.*` loggers
         },
     },
-    'loggers': {
-        'root': {
-            'level': 'DEBUG',
-            'handlers': ['stdout'],
-        },
-        'sipa': {
-            'level': 'DEBUG',
-            'handlers': ['stdout'],
-            'propagate': True,  # Important for `sipa.*` loggers
-        },
-    }
 }
 
 WARNINGS_ONLY_CONFIG = {
