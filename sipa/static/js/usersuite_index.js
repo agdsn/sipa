21ad02bc2d781248fac65f88ba64842faf312265
Gregor Düster <git@gdstr.eu>
2024-02-18 22:57:46 +0100

    Replace Tocbot with functionality from Bootstrap
    Why ship Tocbot if Bootstrap can do it already?
    
    Also see https://getbootstrap.com/docs/5.3/components/scrollspy/ for
    reference.

--- sipa/static/js/usersuite_index.js
+++ sipa/static/js/usersuite_index.js
@@ -1,17 +1,3 @@
-tocbot.init({
-  // Where to render the table of contents.
-  tocSelector: '.js-toc',
-  // Where to grab the headings to build the table of contents.
-  contentSelector: '.js-toc-content',
-  // Which headings to grab inside of the contentSelector element.
-  headingSelector: 'h2, h3',
-  // For headings inside relative or absolute positioned containers within content.
-  hasInnerContainers: true,
-
-  positionFixedSelector: '#usersuite-sidebar-nav',
-  extraLinkClasses: 'text-decoration-none'
-});
-
 // Enable popovers, see https://getbootstrap.com/docs/5.3/components/popovers/
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
