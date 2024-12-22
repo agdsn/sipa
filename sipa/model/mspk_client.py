e682b6eec82f97e22b5566510f245e4773274289
Alex <robokatze182@gmx.de>
2024-12-22 23:47:35 +0100

    added model representing mpsks clients
    added a model for sipa representing mpsks clients
    #498

---
+++ sipa/model/mspk_client.py
@@ -0,0 +1,11 @@
+
+
+class MPSKClientEntry:
+    mac: str
+    name: str
+    id: int
+
+    def __init__(self, mac: str, name: str, id: int):
+        self.mac = mac
+        self.name = name
+        self.id = id
