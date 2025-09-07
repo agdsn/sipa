5b61f51b1236456533cbcd52e21b9a16b690368b
Lukas Juhrich <lukas.juhrich@agdsn.de>
2025-09-07 12:50:53 +0200

    Remove unused parameter from create_app

--- sipa/__init__.py
+++ sipa/__init__.py
@@ -3,9 +3,7 @@ from flask import Flask
 from sipa.initialization import init_app
 
 
-def create_app(app=None, prepare_callable=None, **kwargs):
+def create_app(app=None, **kwargs):
     app = app if app else Flask(__name__)
-    if prepare_callable:
-        prepare_callable(app=app)
     init_app(app, **kwargs)
     return app
