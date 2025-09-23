9f0c6b353733eb2dbe0a20e1667ca4b7ff8aeb7f
Lukas Juhrich <lukas.juhrich@agdsn.de>
2025-09-23 20:19:32 +0200

    Fix error message in babel_utils

--- sipa/utils/babel_utils.py
+++ sipa/utils/babel_utils.py
@@ -3,7 +3,7 @@ from flask_babel import get_locale
 
 def lang():
     l = get_locale()
-    assert l is not None, "get_weekday() must be called inside a request context"
+    assert l is not None, "lang() must be called inside a request context"
     return str(l)
 
 
