5a8ca58c17b564f6faeaf6adcb36308dcd512f68
Lukas Juhrich <lukas.juhrich@agdsn.de>
2025-09-23 20:19:32 +0200

    fix has_connection typing

--- sipa/model/misc.py
+++ sipa/model/misc.py
@@ -1,19 +1,30 @@
+import typing as t
 from collections import namedtuple
 
 from flask import request
 from flask_login import current_user
 
 from sipa.backends import backends
+from sipa.backends.types import UserLike
+if t.TYPE_CHECKING:
+    from .user import BaseUser
+else:
+    class BaseUser: ...
 
 TransactionTuple = namedtuple('Transaction', ['datum', 'value'])
 
 PaymentDetails = namedtuple('PaymentDetails', 'recipient bank iban bic purpose')
 
 
-def has_connection(user):
+def has_connection(user: UserLike) -> bool:
+    if not isinstance(user, BaseUser):
+        return False
     return user.is_authenticated and user.has_connection
 
 
-def should_display_traffic_data():
-    return has_connection(current_user) or has_connection(
-        backends.user_from_ip(request.remote_addr or ""))
+def should_display_traffic_data() -> bool:
+    if has_connection(t.cast(BaseUser, current_user)):
+        return True
+    if not (ip := request.remote_addr):
+        return False
+    return (u := backends.user_from_ip(ip)) is not None and has_connection(u)
