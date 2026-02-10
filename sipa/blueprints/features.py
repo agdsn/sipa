99cef9c35ab4215ae8940c09c8a723a5b6f47290
Lukas Juhrich <lukas.juhrich@agdsn.de>
2026-02-10 20:29:57 +0100

    Make `features.bustimes` work

--- sipa/blueprints/features.py
+++ sipa/blueprints/features.py
@@ -2,11 +2,11 @@
 Basically, this is everything that is to specific to appear in the generic.py
 and does not fit into any other blueprint such as “documents”.
 """
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from flask import Blueprint, current_app, render_template, render_template_string
 
-from sipa.deps import Templates
+from sipa.deps import Templates, Settings
 from sipa.utils import get_bustimes, meetingcal, support_cal, support_hotline_available
 
 bp_features = Blueprint('features', __name__)
@@ -31,6 +31,28 @@ def bustimes(stopname=None):
             data[stop] = get_bustimes(stop, 4)
 
     return render_template('bustimes.html', stops=data, stopname=stopname)
+
+
+@router_features.get("/bustimes")
+@router_features.get("/bustimes/{stopname}")
+def bustimes_(
+    templates: Templates,
+    request: Request,
+    settings: Settings,
+    stopname: str | None = None,
+):
+    """Queries the VVO-Online widget for the given stop.
+    If no specific stop is given in the URL, it will query all
+    stops set up in the config.
+    """
+    data: dict[str, str] = {stopname: get_bustimes(stopname)} if stopname else {
+        stop: get_bustimes(stop, 4) for stop in settings.busstops
+    }
+    return templates.TemplateResponse(
+        request,
+        "bustimes.html",
+        dict(stops=data, stopname=stopname),
+    )
 
 
 @bp_features.route("/meetingcal")
