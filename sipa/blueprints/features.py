ec530091add016197e58a3deb7dca9cee7723a02
Lukas Juhrich <lukas.juhrich@agdsn.de>
2026-02-14 16:42:03 +0100

    Use `tp` for `templates` to disambiguate from `typing as t`

--- sipa/blueprints/features.py
+++ sipa/blueprints/features.py
@@ -2,7 +2,7 @@
 Basically, this is everything that is to specific to appear in the generic.py
 and does not fit into any other blueprint such as “documents”.
 """
-from typing import Iterable
+import typing as t
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from flask import Blueprint, render_template_string
@@ -17,7 +17,7 @@ router_features = APIRouter(default_response_class=HTMLResponse)
 @router_features.get("/bustimes")
 @router_features.get("/bustimes/{stopname}")
 def bustimes(
-    t: Templates,
+    tp: Templates,
     r: Request,
     s: Settings,
     stopname: str | None = None,
@@ -26,18 +26,18 @@ def bustimes(
     If no specific stop is given in the URL, it will query all
     stops set up in the config.
     """
-    stops: dict[str, Iterable[TimeTable]] = (
+    stops: dict[str, t.Iterable[TimeTable]] = (
         {stopname: get_bustimes(stopname)}
         if stopname
         else {stop: get_bustimes(stop, 4) for stop in s.busstops}
     )
-    return t.TemplateResponse(r, "bustimes.html", {"stops": stops})
+    return tp.TemplateResponse(r, "bustimes.html", {"stops": stops})
 
 
 @router_features.get("/meetingcal", name="features.render_meetingcal")
-def render_meetingcal(t: Templates, r: Request, s: Settings):
+def render_meetingcal(tp: Templates, r: Request, s: Settings):
     # TODO turn into proper component
-    return t.TemplateResponse(
+    return tp.TemplateResponse(
         request=r,
         name="meetingcal.html",
         context={"meetings": meetingcal(url=(s.meetings_ical_url))}
@@ -45,12 +45,12 @@ def render_meetingcal(t: Templates, r: Request, s: Settings):
 
 
 @router_features.get("/meetings-fragment", name="features.meetings")
-def meetings_fragment(t: Templates, r: Request, s: Settings):
+def meetings_fragment(tp: Templates, r: Request, s: Settings):
     # TODO turn into proper component
     # TODO think about `FragmentResponse` or some other helper which
     # - when sent with `HX-Request: true`: only sends the fragment
     # - otherwise: embeds it in a default “fragment” presenter
-    return t.TemplateResponse(
+    return tp.TemplateResponse(
         request=r,
         name="meetingcal-fragment.html",
         context={"meetingcal": meetingcal(url=s.meetings_ical_url)},
@@ -58,8 +58,8 @@ def meetings_fragment(t: Templates, r: Request, s: Settings):
 
 
 @router_features.get("/support-fragment", name="features.support_office")
-def support_office(t: Templates, r: Request, s: Settings):
-    return t.TemplateResponse(
+def support_office(tp: Templates, r: Request, s: Settings):
+    return tp.TemplateResponse(
         r,
         "support-fragment.html",
         context={"supports": support_cal(s)},
@@ -78,8 +78,8 @@ def hotline():
 
 
 @router_features.get("/hotline-fragment", name="features.hotline")
-def hotline_(t: Templates, request: Request, s: Settings):
-    return t.TemplateResponse(
+def hotline(tp: Templates, request: Request, s: Settings):
+    return tp.TemplateResponse(
         request,
         "hotline-fragment.html",
         context={"available": support_hotline_available(uri=str(s.pbx_uri))},
